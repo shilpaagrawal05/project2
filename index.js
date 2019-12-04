cefccfcbfe5ed42ef951c78137f69ee1ade66895
CarlTondo9 <54605982+CarlTondo9@users.noreply.github.com>
2019-12-03 19:22:07 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
-app.set('view engine', , 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
 	response.render('pages/index');
